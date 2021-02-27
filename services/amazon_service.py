e846fe34d6760b850cc3c2ac585450e238140db5
estefamr97 <estefaniamurillor@gmail.com>
2021-02-27 11:12:18 -0600

    Export data to excel file

--- services/amazon_service.py
+++ services/amazon_service.py
@@ -6,10 +6,8 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 
-def get_product_data():
-    # print(product['asin'])
-    # url = 'http://www.amazon.com/gp/product/' + product['asin']
-    url = 'http://www.amazon.com/gp/product/' + 'B01FZRK3WW'
+def get_product_data(product):
+    url = 'http://www.amazon.com/gp/product/' + product['asin']
 
     HEADERS = (
         {
@@ -20,8 +18,6 @@ def get_product_data():
 
     html = requests.get(url, headers=HEADERS)
     soup = BeautifulSoup(html.content, features="lxml")
-
-    print(soup)
 
     # Product information
     name = soup.find(id='productTitle').get_text().strip()
@@ -34,15 +30,23 @@ def get_product_data():
         qa = qaHtml[0].find_all('span')[0].get_text()
         qa = int(''.join(filter(str.isdigit, qa)))
     else:
-        qa = 'No Questions or Answers'
+        qa = ''
 
-    review_score = float(
-        soup.select('i[class*="a-icon a-icon-star a-star-"]')[
-            0].get_text().split(' ')[0].replace(",", ".")
-    )
+    score = soup.select('i[class*="a-icon a-icon-star a-star-"]')
 
-    review_count = int(soup.select('#acrCustomerReviewText')[
-                       0].get_text().split(' ')[0].replace(".", ""))
+    if score:
+        review_score = float(
+            score[0].get_text().split(' ')[0].replace(",", ".")
+        )
+    else:
+        review_score = ''
+    
+    count = soup.select('#acrCustomerReviewText')
+
+    if count:
+        review_count = int(count[0].get_text().split(' ')[0].replace(".", "").replace(',',""))
+    else:
+        review_count = ''
 
     try:
         soup.select('#availability .a-color-state')[0].get_text().strip()
@@ -54,69 +58,66 @@ def get_product_data():
     categoriesHtml = soup.find_all("a", {"href": regex})
     categories = re.findall('#\d+\,*\d+', str(categoriesHtml[0].parent))
 
-    print(f'Name = {name}')
-    print(f'ASIN = {asin}')
-    print(f'Review_Score = {review_score}')
-    print(f'Review_Count = {review_count}')
-    print(f'QA = {qa}')
-    print(f'Stock = {stock}')
+    # print(f'Name = {name}')
+    # print(f'ASIN = {asin}')
+    # print(f'Review_Score = {review_score}')
+    # print(f'Review_Count = {review_count}')
+    # print(f'QA = {qa}')
+    # print(f'Stock = {stock}')
     print(f'Categories = {categories}')
+    print(f'Size categories ={len(categories)}')
 
-    # ********************************************
-    # print("antes del data")
-    # data = {
-    #     'Account': product['account'],
-    #     'SKU': product['sku'],
-    #     'Name': name if name is not None else '',
-    #     'Status': 'active',
-    #     'ASIN': product['asin'],
-    #     'Customer Reviews': review_count if review_count is not None else '',
-    #     'Q & A': qa if qa is not None else '',
-    #     'Reviews Rating': review_score if review_score is not None else '',
-    #     'Category': '',
-    #     'Sub. Cat': '',
-    #     'Sub.Cat2': '',
-    #     'Available/Unavailable': stock,
-    # }
-    # print(data)
-    # missing = []
-    # for key, value in data.items():
-    #     if value is None or value == '':
-    #         missing.append(key)
-    # print(missing)
-    # if missing:
-    #     data['Comments'] = 'Lose ' + ', '.join(missing)
-    # else:
-    #     data['Comments'] = ''
+    print("antes del data")
+    data = {
+        'Account': product['account'],
+        'SKU': product['sku'],
+        'Name': name if name is not None else '',
+        'Status': 'Active',
+        'ASIN': product['asin'],
+        'Customer Reviews': review_count if review_count is not None else '',
+        'Q & A': qa if qa is not None else '',
+        'Reviews Rating': review_score if review_score is not None else '',
+        'Category': categories[0].replace('#','') if categories else '',
+        'Sub. Cat': categories[1].replace('#','') if len(categories) >= 2  else '',
+        'Sub.Cat2': categories[2].replace('#','') if len(categories) >= 3 else '',
+        'Available/Unavailable': stock,
+    }
 
-    # return data
-    # *************************************
+    missing = []
+    for key, value in data.items():
+        if value is None or value == '':
+            missing.append(key)
+
+    if missing:
+        data['Comments'] = 'Lose ' + ', '.join(missing)
+    else:
+        data['Comments'] = ''
+
+    return data
 
     # 'https://towardsdatascience.com/scraping-multiple-amazon-stores-with-python-5eab811453a8'
     # 'https://www.youtube.com/watch?v=Bg9r_yLk7VY&feature=youtu.be'
 
 
 def create_report():
-    get_product_data("product")
-    # df = pd.DataFrame(
-    #     columns=[
-    #         'Account', 'SKU', 'Name', 'Status', 'ASIN', 'Customer Reviews',
-    #         'Q & A', 'Reviews Rating', 'Category', 'Sub. Cat', 'Sub.Cat2',
-    #         'Avaliable/Unavaliable', 'Comments'
-    #     ]
-    # )
-    # data = {}
-    # try:
-    #     start = time.time()
-    #     print(start)
-    #     with open('./reports/products2.json') as json_file:
-    #         data = json.load(json_file)
-    #     for product in data['products']:
-    #         print(product)
-    #         df = df.append(get_product_data(product), ignore_index=True)
-    #         time.sleep(2)
-    #     df.to_excel("./reports/asin2.xlsx", index=False)
-    #     print(f'{time.time() - start}.:2')
-    # except Exception as e:
-    #     print("except")
-    #     print(e)
+    df = pd.DataFrame(
+        columns=[
+            'Account', 'SKU', 'Name', 'Status', 'ASIN', 'Customer Reviews',
+            'Q & A', 'Reviews Rating', 'Category', 'Sub. Cat', 'Sub.Cat2',
+            'Available/Unavailable', 'Comments'
+        ]
+    )
+    data = {}
+    try:
+
+        with open('./reports/products2.json') as json_file:
+            data = json.load(json_file)
+        for product in data['products']:
+            df = df.append(get_product_data(product), ignore_index=True)
+            time.sleep(2)
+
+        df.to_excel("./reports/asin2.xlsx", index=False)
+
+    except Exception as e:
+        print("except")
+        print(e)
