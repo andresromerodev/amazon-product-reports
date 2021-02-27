234fd6b4885973389bca18dd11d6377a3c7f5b93
ANDRESROMEROH <andres.romero22@outlook.com>
2021-02-26 21:02:00 -0600

    Reverting changes

--- services/amazon_service.py
+++ services/amazon_service.py
@@ -13,12 +13,8 @@ def get_product_data():
 
     HEADERS = (
         {
-            'Accept-Encoding': 'gzip, deflate, sdch',
-            'Accept-Language': 'en-US,en;q=0.8',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Referer': 'http://www.wikipedia.org/',
-            'Connection': 'keep-alive',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'
         }
     )
 
