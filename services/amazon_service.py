ef61a5ffbbff04bb20139c8e1ce2c0c794d7b828
ANDRESROMEROH <andres.romero22@outlook.com>
2021-02-27 18:57:01 -0600

    Fixed categories

--- services/amazon_service.py
+++ services/amazon_service.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 
 def get_product_data(product):
-    url = 'http://www.amazon.com/gp/product/' + product['asin']
+    url = 'http://www.amazon.com/dp/product/' + product['asin']
 
     HEADERS = (
         {
@@ -40,34 +40,40 @@ def get_product_data(product):
         )
     else:
         review_score = ''
-    
+
     count = soup.select('#acrCustomerReviewText')
 
     if count:
-        review_count = int(count[0].get_text().split(' ')[0].replace(".", "").replace(',',""))
+        review_count = int(count[0].get_text().split(
+            ' ')[0].replace(".", "").replace(',', ""))
     else:
         review_count = ''
 
+    # checking if there is "Out of stock"
     try:
-        soup.select('#availability .a-color-state')[0].get_text().strip()
-        stock = 'Unavailable'
-    except:
+        soup.select('#exportAlternativeContentTitle')[0].get_text().strip()
         stock = 'Available'
+    except:
+        # if there is any error in the previous try statements, it means the product is available
+        stock = 'Out of Stock'
 
     regex = re.compile('.*/bestsellers/.*')
     categoriesHtml = soup.find_all("a", {"href": regex})
-    categories = re.findall('#\d+\,*\d+', str(categoriesHtml[0].parent))
 
-    # print(f'Name = {name}')
-    # print(f'ASIN = {asin}')
-    # print(f'Review_Score = {review_score}')
-    # print(f'Review_Count = {review_count}')
-    # print(f'QA = {qa}')
-    # print(f'Stock = {stock}')
+    if "Best Sellers Rank" not in str(categoriesHtml[0].parent):
+        categories = re.findall('#\d+\,*\d+', str(categoriesHtml[1].parent))
+    else:
+        categories = re.findall('#\d+\,*\d+', str(categoriesHtml[0].parent))
+
+    print(f'Name = {name}')
+    print(f'ASIN = {asin}')
+    print(f'Review_Score = {review_score}')
+    print(f'Review_Count = {review_count}')
+    print(f'QA = {qa}')
+    print(f'Stock = {stock}')
     print(f'Categories = {categories}')
-    print(f'Size categories ={len(categories)}')
+    print(f'Size categories = {len(categories)}')
 
-    print("antes del data")
     data = {
         'Account': product['account'],
         'SKU': product['sku'],
@@ -77,9 +83,9 @@ def get_product_data(product):
         'Customer Reviews': review_count if review_count is not None else '',
         'Q & A': qa if qa is not None else '',
         'Reviews Rating': review_score if review_score is not None else '',
-        'Category': categories[0].replace('#','') if categories else '',
-        'Sub. Cat': categories[1].replace('#','') if len(categories) >= 2  else '',
-        'Sub.Cat2': categories[2].replace('#','') if len(categories) >= 3 else '',
+        'Category': categories[0].replace('#', '') if categories else '',
+        'Sub. Cat': categories[1].replace('#', '') if len(categories) >= 2 else '',
+        'Sub.Cat2': categories[2].replace('#', '') if len(categories) >= 3 else '',
         'Available/Unavailable': stock,
     }
 
@@ -114,7 +120,7 @@ def create_report():
             data = json.load(json_file)
         for product in data['products']:
             df = df.append(get_product_data(product), ignore_index=True)
-            time.sleep(2)
+            time.sleep(0.5)
 
         df.to_excel("./reports/asin2.xlsx", index=False)
 
